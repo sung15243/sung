ac7c7bc9172dadd685162cff5f7a6551964c08ef
sung15243 <sn2025075@shs.hs.kr>
2025-06-12 12:44:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ st.set_page_config(page_title="📰 가짜 뉴스 메이커", page_icon="🗞️
 st.title("🗞️ 가짜 뉴스 메이커")
 st.write("콤마(,)로 키워드 3개 이상 입력하면 웃긴 가짜 뉴스 헤드라인을 만들어 드립니다!")
 
-# 뉴스 헤드라인 템플릿 (키워드 3개 사용)
 HEADLINE_TEMPLATES = [
     "⚡ 충격! {0}가 {1}에서 {2}를 발견했다?!",
     "🔍 전문가들, {0}와 {1}의 관계에 {2}가 결정적 역할을 했다고 밝혀",
@@ -18,13 +17,11 @@ HEADLINE_TEMPLATES = [
 ]
 
 def generate_headline(keywords):
-    # 키워드가 3개 미만이면 랜덤 중복으로 채우기
     while len(keywords) < 3:
         keywords.append(random.choice(keywords))
     template = random.choice(HEADLINE_TEMPLATES)
     return template.format(keywords[0], keywords[1], keywords[2])
 
-# 키워드 입력란
 user_input = st.text_input("키워드를 입력하세요 (콤마로 구분):", placeholder="예: 우주인, 치킨, 대통령")
 
 if st.button("📰 가짜 뉴스 만들기"):
